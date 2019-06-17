ce863a114dcfb2476ba8e10e06aed7f20dd72231
1ba1 <vvvalenti01@yahoo.it>
2019-06-17 11:43:42 +0200

    try to implement updateArray

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,20 +1,38 @@
 const board = (() => {
-  const arr = ["X","X","X","X","X","X","X","X","X"];
-  const updateBoard = () => {
-    arr.forEach(marker => {});
+  let arr = [];
+  const initializeBoard = () => {
+    for (let i = 0; i < 9; i++) {
+      arr.push('');
+      const div = document.getElementById(i);
+      const boundFunction = () => {
+        updateArray.bind(i)
+      }
+      div.addEventListener('click', boundFunction, false);
+    }
   };
-  return { arr, updateBoard }; 
+  const updateBoard = () => {
+    arr.forEach((marker, index) => {
+      const div = document.getElementById(index);
+      div.textContent = marker;
+    });
+  };
+  const updateArray = markerI => {
+    console.log(markerI)
+  };
+  return { arr, initializeBoard, updateBoard };
 })();
 
 const displayController = (() => {
-  
+
   return { };
 })();
 
 const player = (name, marker) => {
-  
   return { name, marker };
 };
 
 const player1 = player("Player 1", "X");
 const player2 = player("Player 2", "O");
+
+board.initializeBoard();
+board.updateBoard();
