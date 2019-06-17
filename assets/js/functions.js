5b7360563bfffccbc9b7fdb1312331d9de06deb6
1ba1 <vvvalenti01@yahoo.it>
2019-06-17 17:05:14 +0200

    finish project

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -18,10 +18,10 @@ const displayController = (() => {
       div.textContent = marker;
     });
   };
-  const setMessage = (msg) => {
+  const setMessage = msg => {
     const messagesDiv = document.getElementById("messages");
     messagesDiv.textContent = msg;
-  }
+  };
   return { updateBoard, setMessage };
 })();
 
@@ -29,10 +29,19 @@ const gameLogic = (() => {
   let activeTurn = 0;
   let players = [];
 
-  const setPlayers = (arr) => {
+  const setPlayers = arr => {
     players = arr;
-  }
-  const winCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+  };
+  const winCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
   const startGame = () => {
     for (let i = 0; i < 9; i++) {
       board.arr.push("");
@@ -50,7 +59,11 @@ const gameLogic = (() => {
   const win = () => {
     for (let i = 0; i < winCombos.length; i++) {
       let first = board.arr[winCombos[i][0]];
-      if (first !== "" && first === board.arr[winCombos[i][1]] && first === board.arr[winCombos[i][2]]) {
+      if (
+        first !== "" &&
+        first === board.arr[winCombos[i][1]] &&
+        first === board.arr[winCombos[i][2]]
+      ) {
         return true;
       }
     }
@@ -69,19 +82,20 @@ const gameLogic = (() => {
       displayController.setMessage(players[activeTurn].name + "'s turn!");
     }
   };
-  const stopGame = (msg) => {
+  const stopGame = msg => {
     displayController.setMessage(msg);
-    const cells = document.getElementsByClassName('cell');
+    const cells = document.getElementsByClassName("cell");
     [...cells].forEach(element => {
       element.removeEventListener("click", play, false);
     });
-    const restartBtn = document.getElementById('restartBtn');
+    const restartBtn = document.getElementById("restartBtn");
     restartBtn.classList.toggle("hide");
-    restartBtn.onclick = () => { location.reload(); }
+    restartBtn.onclick = () => {
+      location.reload();
+    };
   };
   return { startGame, setPlayers };
 })();
-
 
 const player1 = player("Player 1", "X");
 const player2 = player("Player 2", "O");
