d78849553d26c1754d993c53b495afb3da44d068
1ba1 <vvvalenti01@yahoo.it>
2019-06-17 15:15:55 +0200

    define game logic module

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,16 +1,25 @@
+const player = (name, marker) => {
+  const makeMove = (cellId) => {
+    board.arr[cellId] = marker
+  }
+  return { name, marker, makeMove };
+};
+
+const player1 = player("Player 1", "X");
+const player2 = player("Player 2", "O");
+
 const board = (() => {
   let arr = [];
   const initializeBoard = () => {
     for (let i = 0; i < 9; i++) {
       arr.push('');
       const div = document.getElementById(i);
-      div.onclick = updateCell;
+      div.addEventListener('click', gameLogic.play, false);
     }
   };
-  const updateCell = () => {
+  const updateArray = marker => {
     const id = event.srcElement.id;
-    arr[id] = "H";
-    updateBoard();
+    arr[id] = marker;
   }
   const updateBoard = () => {
     arr.forEach((marker, index) => {
@@ -18,34 +27,35 @@ const board = (() => {
       div.textContent = marker;
     });
   };
-  return { arr, initializeBoard, updateBoard };
+  return { arr, initializeBoard, updateBoard, updateArray };
 })();
 
-const displayController = (() => {
-
-  return { };
+const gameLogic = (() => {
+  let activeTurn = 0;
+  let players = [player1, player2];
+  const togglePlayer = () => {
+    activeTurn = activeTurn === 0 ? 1 : 0;
+  };
+  const play = (e) => {
+    board.updateArray(players[activeTurn].marker);
+    board.updateBoard();
+    togglePlayer();
+    e.target.removeEventListener('click', play, false);
+  };
+  return { play };
 })();
 
-const player = (name, marker) => {
-  const markCell = (cell) => {
-    cell.textContent = marker;
-  }
-  return { name, marker, markCell };
-};
 
-const player1 = player("Player 1", "X");
-const player2 = player("Player 2", "O");
-
-// board.initializeBoard();
+board.initializeBoard();
 // board.updateBoard();
 
-const play = (() => {
-  let moves = 0;
-  while (moves < 9) {
-    [player1, player2].forEach( player => {
-      console.log(player.marker);
-      //Player makes move
-      moves++;
-    });
-  }
-})();
+// const play = (() => {
+//   let moves = 0;
+//   while (moves < 9) {
+//     [player1, player2].forEach(player => {
+//       displayController.setCurrentPlayer(player);
+//       //Player makes move
+//       moves++;
+//     });
+//   }
+// })();
